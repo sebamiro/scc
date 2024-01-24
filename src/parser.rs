4dcb37c749395840cce788c6221839b8064da97c
sebamiro <seba.mi.21@gmail.com>
2024-01-24 18:48:53 +0100

    Updated to transfer ownership of Tokens to the ast instead of cloning

--- src/parser.rs
+++ src/parser.rs
@@ -1,15 +1,16 @@
+use std::collections::VecDeque;
+
 use crate::ast;
 use crate::lexer;
+use crate::token::Token;
 
 pub struct Parser {
-    pub tokens: Vec<lexer::Token>,
-    pub current: usize,
+    pub tokens: VecDeque<Token>,
 }
 
 pub fn new(filename: &str) -> Result<Parser, String> {
     Ok(Parser {
         tokens: lexer::lex(filename).map_err(|e| e.to_string())?,
-        current: 0,
     })
 }
 
@@ -20,14 +21,14 @@ impl Parser {
 
     fn parse_function(
         &mut self,
-    ) -> Result<(lexer::Token, Vec<lexer::Token>, Vec<ast::Statement>), String> {
+    ) -> Result<(Token, Vec<Token>, Vec<ast::Statement>), String> {
         println!("Funtion:");
-        let func_type = self.consume(lexer::Token::Int, "Expected function type")?;
+        let func_type = self.consume(Token::Int, "Expected function type")?;
         println!("type: {:?}", func_type);
         let func_name = self.consume_ident("Expected identifier")?;
         println!("name: {:?}", func_name);
-        let _ = self.consume(lexer::Token::LeftParen, "Expected function type")?;
-        let _ = self.consume(lexer::Token::RightParen, "Expected function type")?;
+        let _ = self.consume(Token::LeftParen, "Expected function type")?;
+        let _ = self.consume(Token::RightParen, "Expected function type")?;
         // TODO: parse parameters
         println!("params: []");
         let statements = self.parse_block()?;
@@ -39,13 +40,13 @@ impl Parser {
         match self.peek() {
             None => return Err("Unexpected end of file".to_string()),
             Some(token) => {
-                if lexer::Token::LeftBrace != *token {
+                if Token::LeftBrace != *token {
                     return Err("Unexpected end of file".to_string());
                 }
                 self.advance();
                 let mut statements: Vec<ast::Statement> = Vec::new();
                 while let Some(token) = self.peek() {
-                    if lexer::Token::RightBrace == *token {
+                    if Token::RightBrace == *token {
                         self.advance();
                         return Ok(statements);
                     }
@@ -59,17 +60,15 @@ impl Parser {
     fn parse_statement(&mut self) -> Result<ast::Statement, String> {
         match self.peek() {
             None => return Err("Unexpected end of file".to_string()),
-            Some(token) => {
-                match token {
-                    lexer::Token::Return => {
-                        self.advance();
-                        let expr = self.parse_expression()?;
-                        self.consume(lexer::Token::Semicolon, "Expected ';' after return")?;
-                        return Ok(ast::Statement::Return((lexer::Token::Return, expr)));
-                    }
-                    _ => return Err("Unexpected end of file".to_string()),
+            Some(token) => match token {
+                Token::Return => {
+                    self.advance();
+                    let expr = self.parse_expression()?;
+                    self.consume(Token::Semicolon, "Expected ';' after return")?;
+                    return Ok(ast::Statement::Return((Token::Return, expr)));
                 }
-            }
+                _ => return Err("Unexpected end of file".to_string()),
+            },
         }
     }
 
@@ -77,46 +76,43 @@ impl Parser {
         match self.peek() {
             None => return Err("Unexpected end of file".to_string()),
             Some(token) => match token {
-                lexer::Token::Semicolon => return Ok(None),
-                lexer::Token::Number(_) => return Ok(Some(ast::Expression::Literal(self.advance().clone()))),
+                Token::Semicolon => return Ok(None),
+                Token::Number(_) => {
+                    return Ok(Some(ast::Expression::Literal(self.advance())))
+                }
                 _ => return Err("Unexpected end of file".to_string()),
             },
         }
     }
 
-    fn consume(
-        &mut self,
-        token_type: lexer::Token,
-        message: &str,
-    ) -> Result<lexer::Token, String> {
+    fn consume(&mut self, token_type: Token, message: &str) -> Result<Token, String> {
         if self.check(token_type) {
-            return Ok(self.advance().clone());
+            return Ok(self.advance());
         }
         Err(message.to_string())
     }
 
-    fn consume_ident(&mut self, message: &str) -> Result<lexer::Token, String> {
+    fn consume_ident(&mut self, message: &str) -> Result<Token, String> {
         if let Some(token) = self.peek() {
-            if let lexer::Token::Identifier(_) = token {
-                return Ok(self.advance().clone());
+            if let Token::Identifier(_) = token {
+                return Ok(self.advance());
             }
         }
         Err(message.to_string())
     }
 
-    fn advance(&mut self) -> &lexer::Token {
-        self.current += 1;
-        &self.tokens[self.current - 1]
+    fn advance(&mut self) -> Token {
+        self.tokens.pop_front().unwrap()
     }
 
-    fn check(&self, token_type: lexer::Token) -> bool {
+    fn check(&self, token_type: Token) -> bool {
         if let Some(token) = self.peek() {
             return *token == token_type;
         }
         false
     }
 
-    fn peek(&self) -> Option<&lexer::Token> {
-        self.tokens.get(self.current)
+    fn peek(&self) -> Option<&Token> {
+        self.tokens.get(0)
     }
 }
