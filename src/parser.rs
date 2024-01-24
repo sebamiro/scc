e839d4757d82f5ec4198362bb6458295fa0621b0
sebamiro <seba.mi.21@gmail.com>
2024-01-24 19:15:10 +0100

    Refactor

--- src/parser.rs
+++ src/parser.rs
@@ -19,16 +19,14 @@ impl Parser {
         Ok(ast::Program::Function(self.parse_function()?))
     }
 
-    fn parse_function(
-        &mut self,
-    ) -> Result<(Token, Vec<Token>, Vec<ast::Statement>), String> {
+    fn parse_function(&mut self) -> Result<(Token, Vec<Token>, Vec<ast::Statement>), String> {
         println!("Funtion:");
         let func_type = self.consume(Token::Int, "Expected function type")?;
         println!("type: {:?}", func_type);
         let func_name = self.consume_ident("Expected identifier")?;
         println!("name: {:?}", func_name);
-        let _ = self.consume(Token::LeftParen, "Expected function type")?;
-        let _ = self.consume(Token::RightParen, "Expected function type")?;
+        self.consume(Token::LeftParen, "Expected LeftParen")?;
+        self.consume(Token::RightParen, "Expected RightParen")?;
         // TODO: parse parameters
         println!("params: []");
         let statements = self.parse_block()?;
@@ -38,28 +36,28 @@ impl Parser {
 
     fn parse_block(&mut self) -> Result<Vec<ast::Statement>, String> {
         match self.peek() {
-            None => return Err("Unexpected end of file".to_string()),
-            Some(token) => {
-                if Token::LeftBrace != *token {
-                    return Err("Unexpected end of file".to_string());
-                }
-                self.advance();
-                let mut statements: Vec<ast::Statement> = Vec::new();
-                while let Some(token) = self.peek() {
-                    if Token::RightBrace == *token {
-                        self.advance();
-                        return Ok(statements);
+            None => {}
+            Some(token) => match token {
+                Token::LeftBrace => {
+                    self.advance();
+                    let mut statements: Vec<ast::Statement> = Vec::new();
+                    while let Some(token) = self.peek() {
+                        if Token::RightBrace == *token {
+                            self.advance();
+                            return Ok(statements);
+                        }
+                        statements.push(self.parse_statement()?);
                     }
-                    statements.push(self.parse_statement()?);
                 }
-                return Err("Unexpected end of file".to_string());
-            }
+                _ => {}
+            },
         }
+        Err("Unexpected end of file".to_string())
     }
 
     fn parse_statement(&mut self) -> Result<ast::Statement, String> {
         match self.peek() {
-            None => return Err("Unexpected end of file".to_string()),
+            None => Err("Unexpected end of file".to_string()),
             Some(token) => match token {
                 Token::Return => {
                     self.advance();
@@ -67,49 +65,50 @@ impl Parser {
                     self.consume(Token::Semicolon, "Expected ';' after return")?;
                     return Ok(ast::Statement::Return((Token::Return, expr)));
                 }
-                _ => return Err("Unexpected end of file".to_string()),
+                _ => Err("Unexpected end of file".to_string()),
             },
         }
     }
 
     fn parse_expression(&mut self) -> Result<Option<ast::Expression>, String> {
         match self.peek() {
-            None => return Err("Unexpected end of file".to_string()),
+            None => Err("Unexpected end of file".to_string()),
             Some(token) => match token {
-                Token::Semicolon => return Ok(None),
-                Token::Number(_) => {
-                    return Ok(Some(ast::Expression::Literal(self.advance())))
-                }
-                _ => return Err("Unexpected end of file".to_string()),
+                Token::Semicolon => Ok(None),
+                Token::Number(_) => Ok(Some(ast::Expression::Literal(self.advance()))),
+                _ => Err("Unexpected end of file".to_string()),
             },
         }
     }
 
     fn consume(&mut self, token_type: Token, message: &str) -> Result<Token, String> {
         if self.check(token_type) {
-            return Ok(self.advance());
+            Ok(self.advance())
+        } else {
+            Err(message.to_string())
         }
-        Err(message.to_string())
     }
 
     fn consume_ident(&mut self, message: &str) -> Result<Token, String> {
-        if let Some(token) = self.peek() {
-            if let Token::Identifier(_) = token {
-                return Ok(self.advance());
+        match self.peek() {
+            None => Err(message.to_string()),
+            Some(token) => match token {
+                Token::Identifier(_) => Ok(self.advance()),
+                _ => Err(message.to_string()),
             }
         }
-        Err(message.to_string())
     }
 
     fn advance(&mut self) -> Token {
-        self.tokens.pop_front().unwrap()
+        self.tokens.pop_front().expect("advance: Expected token")
     }
 
     fn check(&self, token_type: Token) -> bool {
         if let Some(token) = self.peek() {
-            return *token == token_type;
+            *token == token_type
+        } else {
+            false
         }
-        false
     }
 
     fn peek(&self) -> Option<&Token> {
